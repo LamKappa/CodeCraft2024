0b5f2f534f55a497afbd4fd459fd18daeaac6d61
LamKappa <xc17725067706@163.com>
2024-03-12 19:53:35 +0800

    fix bug about Robot::create/update that ignore unreachable berths

--- Robot.hpp
+++ Robot.hpp
@@ -47,7 +47,7 @@ struct Robot {
                 if(item.occupied) { continue; }
                 if(item.live_time() < Atlas::atlas.distance(exec->pos, item.pos)) { continue; }
                 for(auto &berth: Berths::berths) {
-                    if(berth.disabled) { continue; }
+                    if(berth.disabled || Atlas::atlas.distance(exec->pos, berth.pos) == Atlas::INF_DIS) { continue; }
                     float value = calc_value(*exec, item, berth);
                     if(value > mission.reserved_value) {
                         if(mission.item_id >= 0) {
@@ -87,7 +87,7 @@ struct Robot {
                 auto &item = *itr;
                 if(item.occupied) { continue; }
                 for(auto &berth: Berths::berths) {
-                    if(berth.disabled) { continue; }
+                    if(berth.disabled || Atlas::atlas.distance(executor->pos, berth.pos) == Atlas::INF_DIS) { continue; }
                     float value = calc_value(*executor, item, berth);
                     if(value > reserved_value) {
                         if(item_id >= 0) {
